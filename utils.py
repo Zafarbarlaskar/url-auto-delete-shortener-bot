3b33eea4911d0c19ca579523a4373db47c2ef846
Zafarbarlaskar <96040383+Zafarbarlaskar@users.noreply.github.com>
2022-11-25 16:48:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -395,7 +395,7 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://Shareus/api?api={iLQQbjTVRWabF6jlpZ3wWruTOje2}&link={ https://shareus.io/pub/tools}'
 
     except Exception as e:
         logger.error(e)
